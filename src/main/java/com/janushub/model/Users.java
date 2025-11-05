ff70f4dc9c6ab787e0bf5d865ee9e92b7b73c00b
JJLopez18 <jaguirrre18@gmail.com>
2025-11-05 17:56:46 +0100

    Añadido los models y los repositorys que faltaban

--- src/main/java/com/janushub/model/Users.java
+++ src/main/java/com/janushub/model/Users.java
@@ -1,16 +1,22 @@
 package com.janushub.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime; // Necesitas java.time para las fechas
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "users")
 public class Users {
     @Id
     private String id;
     private String username;
-    private String hash; // Almacena el hash de la contraseña (Ej. $argon2id$...)
+    private String password; // Almacena el hash de la contraseña (Ej. $argon2id$...)
     private List<String> roles; // Ej. ["ADMIN", "DEVOPS"]
     private String fullName;
     private String email;
@@ -18,87 +24,4 @@ public class Users {
     private String status; // Ej. "ACTIVE"
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    
-    public Users() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
