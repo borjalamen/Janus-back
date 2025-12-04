7f753ab58f3a2fc45fcc9a73a3339880dae98e99
borjalamen <63474986+borjalamen@users.noreply.github.com>
2025-12-04 12:47:01 +0100

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Dspring.data.mongodb.uri=${urlMongoDB} -Dspring.data.mongodb.database=${indexMongoDB} -Dspring.data.mongodb.auto-index-creation=${databaseMongoDB} -Dserver.port=${portMongoDB} -Xms${XMS} -Xmx${XMX} app.jar
+java -jar -Dspring.data.mongodb.uri=${urlMongoDB} -Dspring.data.mongodb.database=${databaseMongoDB} -Dspring.data.mongodb.auto-index-creation=${indexMongoDB} -Dserver.port=${portMongoDB} -Xms${XMS} -Xmx${XMX} app.jar
