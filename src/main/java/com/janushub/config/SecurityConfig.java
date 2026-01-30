d4ec7820b92c66c6af0fae8bd531bdd97958311b
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2026-01-30 09:30:18 +0100

    Update SecurityConfig.java

--- src/main/java/com/janushub/config/SecurityConfig.java
+++ src/main/java/com/janushub/config/SecurityConfig.java
@@ -8,6 +8,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            // ❌ desactivar CSRF (necessari per Postman)
+            .csrf(csrf -> csrf.disable())
+
+            // ✅ permetre TOTES les rutes
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+
+            // ❌ desactivar formulari de login
+            .formLogin(form -> form.disable())
+
+            // ❌ desactivar basic auth
+            .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
