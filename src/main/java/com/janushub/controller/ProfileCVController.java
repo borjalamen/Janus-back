caf9f37de61a46836b6258677691be968a245d25
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2026-02-02 09:20:30 +0100

    libreria authentication eliminada

--- src/main/java/com/janushub/controller/ProfileCVController.java
+++ src/main/java/com/janushub/controller/ProfileCVController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.security.core.Authentication;
+
 
 import com.janushub.service.UserService;
 
