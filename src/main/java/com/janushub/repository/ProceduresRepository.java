70e1b90ece69c866f3cae66ead84acff0e509e8e
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2026-02-10 09:31:41 +0100

    problemas de inicializacion debido a mongoDB

--- src/main/java/com/janushub/repository/ProceduresRepository.java
+++ src/main/java/com/janushub/repository/ProceduresRepository.java
@@ -12,4 +12,6 @@ public interface ProceduresRepository extends MongoRepository<Procedure, String>
     
     // Buscar por titulo (contiene texto, case-insensitive) solo activos
     List<Procedure> findByTituloContainingIgnoreCaseAndIsDeletedFalse(String titulo);
+
+     List<Procedure> findByProcedureIdIsNull();
 }
