23c907239aff07f78e4a89371fc2fc214d502a3b
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2026-02-11 09:51:57 +0100

    Revert "graphql clases i funcionalidades"
    This reverts commit 60658eb65e5f0ccffc74e8333596b6dd68f4d696.

--- src/main/java/com/janushub/model/StepRepository.java
+++ src/main/java/com/janushub/model/StepRepository.java
@@ -1,7 +1,5 @@
 package com.janushub.model;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
@@ -10,8 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface StepRepository extends MongoRepository<StepDocumentID, String> {
       StepDocumentID findByStepId(String stepId);
 
-    List<StepDocumentID> findByResponsableContainingIgnoreCase(String responsable);
+    boolean existsByStepId(String stepId);
 
-    List<StepDocumentID> findByTagsContaining(String tag);
     
 }
