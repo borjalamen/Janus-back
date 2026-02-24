5ac15105253dabf36196cd4bf2a9fe68e6b32bdd
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2026-02-24 21:34:57 +0100

    errores peticiontarea duplicados connexion con front

---
+++ src/main/java/com/janushub/repository/PeticionTareaRepository.java
@@ -0,0 +1,12 @@
+package com.janushub.repository;
+
+import com.janushub.model.PeticionTarea;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface PeticionTareaRepository extends MongoRepository<PeticionTarea, String> {
+
+    List<PeticionTarea> findByEstado(String estado);
+
+}
