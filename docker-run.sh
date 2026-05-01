63ded39b34994ae3f6c8e08085956d8aa4807001
fsgilp <fsgilp@gmail.com>
2026-05-01 16:02:56 +0200

    chore: pass GROQ_API_KEY via env in docker-run.sh; ignore .env

--- docker-run.sh
+++ docker-run.sh
@@ -6,6 +6,7 @@ java \
   -Dspring.data.mongodb.database="${databaseMongoDB}" \
   -Dspring.data.mongodb.auto-index-creation="${indexMongoDB}" \
   -Dserver.port="${portMongoDB}" \
+  -Dgroq.api.key="${GROQ_API_KEY}" \
   -Xms"${XMS}" \
   -Xmx"${XMX}" \
   -jar app.jar
